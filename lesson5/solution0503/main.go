4ce0c1ef152ace7ca85915b33af9f8211d3358ad
ArtemCreator <garribond85@gmail.com>
2020-07-18 14:24:13 +0500

    create solution0503, 0504

---
+++ lesson5/solution0503/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	fmt.Println("Введите 3 числа, проверим есть ли совпадающие из них")
+	fmt.Scan(&a, &b, &c)
+
+	if a == b || a == c || b == c{
+		fmt.Println("Есть совпадения чисел")
+	}else {
+		fmt.Println("Нету совпадений")
+	}
+
+	comparing()
+}
+
+func comparing()  {
+	var a, b, c int
+	var count int
+
+	fmt.Println("Введите 3 числа, проверим есть ли совпадающие из них")
+	fmt.Scan(&a, &b, &c)
+
+	if a == b || a == c || b == c{
+		fmt.Print("Совпадают числа: ")
+		if a == b {
+			count++
+			fmt.Printf("%d и %d; ", a, b)
+		}
+		if a == c {
+			count++
+			fmt.Printf("%d и %d; ", a, c)
+		}
+		if b == c {
+			count++
+			fmt.Printf("%d и %d; ", b, c)
+		}
+		fmt.Println(" всего совпадений ", count)
+	}else {
+		fmt.Println("Нету совпадений")
+	}
+}
+
+func init() {
+	fmt.Println("*************Проверить, что есть совпадающие числа***************")
+}
---
+++ lesson5/solution0504/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstM, secondM, thirdM int
+	fmt.Println("Введите номинал монет имеющихся у вас")
+	fmt.Scan(&firstM, &secondM, &thirdM)
+
+	var money int
+	fmt.Println("Введите сумму которую надо оплатить")
+	fmt.Scan(&money)
+
+	if firstM + secondM + thirdM == money{
+		fmt.Println("Денежных средств хватает для оплаты без сдачи")
+	}else if firstM + secondM == money || secondM + thirdM == money|| firstM + secondM == money{
+		fmt.Println("Денежных средств хватает для оплаты без сдачи")
+	}else if firstM == money || secondM == money || thirdM == money {
+		fmt.Println("Денежных средств хватает для оплаты без сдачи")
+	}else {
+		fmt.Println("Денежных средств не хватить для оплаты без сдачи")
+	}
+}
+func init() {
+	fmt.Println("*********Сумма без сдачи**********")
+}
