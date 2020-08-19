81e940126bc3fecedb41729d2ecaa661546883b9
ArtemCreator <garribond85@gmail.com>
2020-08-19 13:45:20 +0500

    create solution0604

---
+++ lesson6/solution0604/main.go
@@ -0,0 +1,19 @@
+// Заполнение трех переменных
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	for c != 1000{
+		a++; b++; c++
+		if a <= 10{
+			fmt.Print("a = ", a)
+		}
+		if b <= 100{
+			fmt.Print("\tb = ", b)
+		}
+		fmt.Println("\tc = ", c)
+	}
+}
