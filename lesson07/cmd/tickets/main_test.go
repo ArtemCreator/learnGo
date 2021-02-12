b947f3ed1d04977dc80e7d307a28624a2335a61b
Artem <garribond85@gmail.com>
2021-02-12 12:24:21 +0500

    added main_test.go

--- lesson07/cmd/tickets/main_test.go
+++ lesson07/cmd/tickets/main_test.go
@@ -1,1 +1,25 @@
 package main
+
+import "testing"
+
+func Test_mirrorTicket(t *testing.T) {
+	type args struct {
+		countTickets int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{name: "ok", args: args{999}, want: 900},
+		{name: "ok2", args: args{450}, want: 351},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mirrorTicket(tt.args.countTickets); got != tt.want {
+				t.Errorf("mirrorTicket() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
