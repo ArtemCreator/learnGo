844f7dc1bdc79093c50daca5710fe29b841f9c43
Artem <garribond85@gmail.com>
2021-02-12 12:22:21 +0500

    create a mirrorTicket func

--- lesson07/cmd/tickets/main.go
+++ lesson07/cmd/tickets/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 )
 
 func main() {
+	countTickets := mirrorTicket(999)
+	fmt.Printf("Total number of mirror tikets: %v\n", countTickets)
+
 	ticMinCount := 100000
 	ticMaxCount := 999999
+	var minHappyTicket = 100000
+	var maxHappyTicket = 100000
+	var minusTic int // разница
+	var maxBuyTicket int
 	var mirrorCount int
 
 	for i := ticMinCount; i <= ticMaxCount; i++ {
@@ -23,9 +32,20 @@ func main() {
 		if n1 == n6 && n2 == n5 && n3 == n4 {
 			mirrorCount++
 		}
+		if n1+n2+n3 == n4+n5+n6 {
+			maxHappyTicket = i
+			minusTic = maxHappyTicket - minHappyTicket
+			minHappyTicket = maxHappyTicket
+			if minusTic > maxBuyTicket {
+				maxBuyTicket = minusTic
+			}
+			//fmt.Println(maxHappyTicket, minusTic)
+		}
 		//fmt.Println(n1, n2, n3, n4, n5, n6)
 	}
-	fmt.Printf("Количество зеркальных билетов %d\n", mirrorCount)
+	log.Printf("Max count a buy tickets: %v", maxBuyTicket)
+	log.Printf("Количество зеркальных билетов %d", mirrorCount)
+
 	var count int
 	mirrorCount = 0
 	for i := 100; i <= 999; i++ {
@@ -42,4 +62,25 @@ func main() {
 	}
 	fmt.Printf("Количество счастливых билетов: %d,\nколичество зеркальных билетов %d\n", count, mirrorCount)
 
+}
+
+// mirrorTicket returns the number of mirror tickets and an error
+func mirrorTicket(countTickets int) int {
+	var mirrorCountTickets int
+	for i := countTickets; i >= 100; i-- {
+		strNumber := strconv.Itoa(i)
+		reverseStrNumber := ""
+		for length := len(strNumber); length > 0; length-- {
+			reverseStrNumber += string(strNumber[length-1])
+		}
+		reversNum, err := strconv.Atoi(reverseStrNumber)
+		if err != nil {
+			log.Println("Failure to cast String to int")
+		}
+		if i/100 == reversNum%10 && i/10%10 == reversNum/10%10 && i%10 == reversNum/100 {
+			mirrorCountTickets++
+		}
+		// fmt.Println(i, reversNum, mirrorCountTickets)
+	}
+	return mirrorCountTickets
 }
