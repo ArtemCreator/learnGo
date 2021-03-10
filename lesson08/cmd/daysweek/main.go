057590ba99fafb7583111b839de44a04188a7bc2
Artem <garribond85@gmail.com>
2021-03-10 12:11:48 +0500

    Created the programm to output days week

---
+++ lesson08/cmd/daysweek/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Введите день недели в сокращенной форме\n (пн, вт, ср, чт, пт)")
+	var dayweek string
+	_, _ = fmt.Scan(&dayweek)
+	switch dayweek{
+	case "пн":
+		fmt.Println("понедельник")
+		fallthrough
+	case "вт":
+		fmt.Println("вторник")
+		fallthrough
+	case "ср":
+		fmt.Println("среда")
+		fallthrough
+	case "чт":
+		fmt.Println("четверг")
+		fallthrough
+	case "пт":
+		fmt.Println("пятница")
+	default:
+		fmt.Println("This word not found")
+	}
+}
